33018b3344d99c0fcbdbdf48ec046622c465dc06
Barri <40701426+barrisam@users.noreply.github.com>
2023-05-28 23:33:44 +0100

    Update sales_dashboard.py

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -55,7 +55,7 @@ with outer_cols[0]:
     # using groupby to extract the columns we need from the dataframe
     plt.style.use("fivethirtyeight")
     
-    region_sales_profit = df.groupby['Region']('Sales', 'Profit').mean()
+    region_sales_profit = df.groupby('Region')[('Sales', 'Profit')].mean()
     region_sales_profit = region_sales_profit.reset_index()
     sales = region_sales_profit['Sales']
     profit = region_sales_profit['Profit']
